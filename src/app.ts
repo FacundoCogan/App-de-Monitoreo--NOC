98b26108abe224c0860ebd4ead492903d0f9c9e4
Facundo <facundo_06@live.com.ar>
2024-01-25 23:24:15 -0300

    Subo app terminada y testeada con jest

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+import { envs } from "./config/plugins/envs.plugin";
+import { MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
 
 
@@ -5,7 +8,62 @@ import { Server } from "./presentation/server";
     await main(); //termina el main antes de hacer otros pasos
 })();
 
-function main () {
-    Server.start();
+async function main () {
 
-}
+
+    await MongoDatabase.connect({
+        mongoUrl:envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+    });
+
+    // const prisma = new PrismaClient();
+    // const newLog = await prisma.logModel.create({
+    //     data: {
+    //         level: 'HIGH',
+    //         message: 'Test message',
+    //         origin: 'App.ts'
+    //     },
+    // });
+
+    // const logs = await prisma.logModel.findMany({
+    //     where: {
+    //         level: 'MEDIUM'
+    //     }
+    // });
+
+    Server.start();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Crear un coleccion = tables, documeno = registro
+    // const newLog = await LogModel.create({
+        //     message: 'Test message from Mongo',
+        //     origin: 'App.ts',
+        //     level: 'low'
+        // });
+        
+        // await newLog.save();
+        // console.log(newLog);
+        
+        // const logs = await LogModel.find();
+        // console.log(logs);
+        
+        
+
+}    
--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,15 +1,19 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
     execute(url:string):Promise<boolean>
 }
 
-type SuccessCallback = () => void; // en caso de exito
-type ErrorCallback = (error:string) => void; // en caso  de fracaso
+type SuccessCallback = (() => void) | undefined; // en caso de exito
+type ErrorCallback = ((error:string) => void) | undefined; // en caso  de fracaso
 
 
 export class CheckService implements CheckServiceUseCase{ // caso de uso = codigo especializado en una tarea
 
-    constructor( //Inyec. Depen. agregarle dependencias a nuestra clase para que el CheckService funcione como espero
-                
+    constructor(
+         //Inyec. Depen. agregarle dependencias a nuestra clase para que el CheckService funcione como espero
+        private readonly logRepository: LogRepository,
         private readonly successCallback:SuccessCallback, //readonly para no cambiarlo
         private readonly errorCallback:ErrorCallback
 
@@ -23,15 +27,28 @@ export class CheckService implements CheckServiceUseCase{ // caso de uso = codig
                 throw new Error(`Error on check service ${url}`);
             }
 
+            const log=new LogEntity({level:LogSeverityLevel.low,
+                message: `Service ${url} working`,
+                origin:'check-service.ts'
+            });
 
-            this.successCallback();
+            this.logRepository.saveLog(log)
+            if(this.successCallback)
+                this.successCallback();
+
             return true;
-
         } catch (error) {
-            console.log(`${error}`)
 
+            const errorMessage = `${url} is not ok. ${error}`;
+            const log=new LogEntity({level:LogSeverityLevel.high,
+                message: errorMessage,
+                origin:'check-service.ts'
+            });
+            this.logRepository.saveLog(log);
 
-            this.errorCallback(`${error}`);
+            if(this.errorCallback)
+                this.errorCallback(errorMessage);
+            
             return false;
         }
         
